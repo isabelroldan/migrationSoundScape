047c250d38a09b1eb5a9474cf7baef5b25d35777
Sergio Cubero <scubcar521@gmail.es>
2023-10-31 13:42:25 +0100

    First Commit

--- src/main/java/com/juanite/model/domain/User.java
+++ src/main/java/com/juanite/model/domain/User.java
@@ -1,4 +1,50 @@
 package com.juanite.model.domain;
 
+import java.util.Objects;
+
 public class User {
+    private int id_user;
+    private String photo;
+
+    public User(int id_user, String photo) {
+        this.id_user = id_user;
+        this.photo = photo;
+    }
+
+    public int getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(int id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id_user=" + id_user +
+                ", photo='" + photo + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id_user == user.id_user && Objects.equals(photo, user.photo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_user, photo);
+    }
 }
