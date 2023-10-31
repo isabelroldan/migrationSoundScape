d5927f5aa598378903757138ef7df9715a6cd3aa
Sergio Cubero <scubcar521@gmail.es>
2023-10-31 13:42:25 +0100

    First Commit

--- src/main/java/com/juanite/model/domain/Admin.java
+++ src/main/java/com/juanite/model/domain/Admin.java
@@ -1,4 +1,39 @@
 package com.juanite.model.domain;
 
+import java.util.Objects;
+
 public class Admin {
+    private int id_Persona;
+
+    public Admin(int id_Persona) {
+        this.id_Persona = id_Persona;
+    }
+
+    public int getId_Persona() {
+        return id_Persona;
+    }
+
+    public void setId_Persona(int id_Persona) {
+        this.id_Persona = id_Persona;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id_Persona=" + id_Persona +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Admin admin = (Admin) o;
+        return id_Persona == admin.id_Persona;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_Persona);
+    }
 }
--- src/main/java/com/juanite/model/domain/Person.java
+++ src/main/java/com/juanite/model/domain/Person.java
@@ -1,4 +1,74 @@
 package com.juanite.model.domain;
 
+import java.util.Objects;
+
 public class Person {
+    private int id_Person;
+    private String name;
+    private String gmail;
+    private String password;
+
+    public Person(int id_Person, String name, String gmail, String password) {
+        this.id_Person = id_Person;
+        this.name = name;
+        this.gmail = gmail;
+        this.password = password;
+    }
+
+
+    public int getId_Person() {
+        return id_Person;
+    }
+
+    public void setId_Person(int id_Person) {
+        this.id_Person = id_Person;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGmail() {
+        return gmail;
+    }
+
+    public void setGmail(String gmail) {
+        this.gmail = gmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id_Person=" + id_Person +
+                ", name='" + name + '\'' +
+                ", gmail='" + gmail + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id_Person == person.id_Person && Objects.equals(name, person.name) && Objects.equals(gmail, person.gmail) && Objects.equals(password, person.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id_Person, name, gmail, password);
+    }
 }
