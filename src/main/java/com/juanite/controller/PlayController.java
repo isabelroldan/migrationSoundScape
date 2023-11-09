7dceb3e0fb5abb9a0d2a46e590eb9750c812d3e7
Jose Espejo <jespagu2610@g.educaand.es>
2023-11-09 19:04:05 +0100

    Album view and controller

--- src/main/java/com/juanite/controller/PlayController.java
+++ src/main/java/com/juanite/controller/PlayController.java
@@ -1,4 +1,19 @@
 package com.juanite.controller;
 
+import com.juanite.model.domain.Song;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class PlayController {
+    @FXML
+    private Label songLabel;
+
+    private Song currentSong;
+
+    public void setSong(Song song) {
+        currentSong = song;
+        songLabel.setText(song.getName());
+        // Lógica para reproducir la canción
+    }
 }
+
