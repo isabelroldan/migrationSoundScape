f3234bbc47c36719959baffe87ba6758eb215882
Juanitte <akacksthelightbringer@gmail.com>
2023-11-12 03:26:02 +0100

    Added comment functionality (not tested)

--- src/main/java/com/juanite/controller/CommentController.java
+++ src/main/java/com/juanite/controller/CommentController.java
@@ -1,26 +1,26 @@
 package com.juanite.controller;
 
 import com.juanite.App;
+import com.juanite.model.DAO.CommentDAO;
 import com.juanite.model.DAO.PlaylistDAO;
 import com.juanite.model.DAO.SongDAO;
+import com.juanite.model.domain.Comment;
 import com.juanite.model.domain.Playlist;
 import com.juanite.model.domain.Song;
 import com.juanite.util.AppData;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-public class PlaylistController {
+public class CommentController {
     @FXML
     public ImageView img_avatar;
     @FXML
@@ -40,21 +40,31 @@ public class PlaylistController {
     @FXML
     public Label messageLabel;
     @FXML
-    public ListView<Song> songListView;
+    public ListView<Comment> commentListView;
     @FXML
-    public Button btn_goSong;
+    public Button btn_addComment;
     @FXML
-    public Button btn_removeSong;
-    @FXML
-    public Button btn_changeName;
+    public Button btn_showComment;
     @FXML
     public Label lbl_plName;
     @FXML
-    public TextField txtfld_changeName;
+    public Label lbl_user;
     @FXML
-    public Button btn_comments;
+    public Label lbl_timestamp;
+    @FXML
+    public Label lbl_comment;
+    @FXML
+    public Button btn_back;
+    @FXML
+    public Label lbl_userTitle;
+    @FXML
+    public Label lbl_timestampTitle;
+    @FXML
+    public Label lbl_commentTitle;
+    @FXML
+    public TextArea txt_newComment;
     private String botonEstiloOriginal;
-    private ObservableList<Song> plSongs;
+    private ObservableList<Comment> plComments;
 
     public void initialize() {
         botonEstiloOriginal = homeButton.getStyle();
@@ -74,69 +84,74 @@ public class PlaylistController {
         profileButton.setOnMouseEntered(event -> changeColorButtonprofile(true));
         profileButton.setOnMouseExited(event -> changeColorButtonprofile(false));
 
-        plSongs = FXCollections.observableArrayList();
-        List<Song> songs = new ArrayList<>();
-        songs.addAll(new SongDAO(new Song()).getByPlaylist(AppData.getCurrentPL()));
-        AppData.getCurrentPL().setSongs(songs);
-        plSongs.addAll(AppData.getCurrentPL().getSongs());
-        songListView.setItems(plSongs);
-        songListView.refresh();
-        lbl_plName.setText(AppData.getCurrentPL().getName());
-        txtfld_changeName.setVisible(false);
-        if(AppData.getCurrentPL().getOwner().getId() == AppData.getCurrentUser().getId()) {
-            btn_changeName.setVisible(true);
+        plComments = FXCollections.observableArrayList();
+        List<Comment> comments = new ArrayList<>();
+        comments.addAll(new CommentDAO(new Comment()).getByPlaylist(AppData.getCurrentPL()));
+        AppData.getCurrentPL().setComments(comments);
+        plComments.addAll(AppData.getCurrentPL().getComments());
+        commentListView.setItems(plComments);
+        commentListView.refresh();
+        lbl_plName.setText(AppData.getCurrentPL().getName() + " Comments");
+        lbl_user.setVisible(false);
+        lbl_timestamp.setVisible(false);
+        lbl_comment.setVisible(false);
+        btn_back.setVisible(false);
+        lbl_userTitle.setVisible(false);
+        lbl_commentTitle.setVisible(false);
+        lbl_timestampTitle.setVisible(false);
+        commentListView.setVisible(true);
+        btn_addComment.setVisible(true);
+        btn_showComment.setVisible(true);
+        txt_newComment.setVisible(false);
+
+    }
+
+    public void showComment() {
+        if(commentListView.getSelectionModel().getSelectedItem() != null) {
+            AppData.setCurrentComment(commentListView.getSelectionModel().getSelectedItem());
+            commentListView.setVisible(false);
+            btn_addComment.setVisible(false);
+            btn_showComment.setVisible(false);
+            lbl_plName.setText(AppData.getCurrentComment().getUser().getName() + " Comment");
+            lbl_user.setText(AppData.getCurrentComment().getUser().getName());
+            lbl_user.setVisible(true);
+            lbl_userTitle.setVisible(true);
+            lbl_timestampTitle.setVisible(true);
+            lbl_commentTitle.setVisible(true);
+            lbl_timestamp.setText(AppData.getCurrentComment().getDate_time().toString());
+            lbl_timestamp.setVisible(true);
+            lbl_comment.setText(AppData.getCurrentComment().getComment());
+            lbl_comment.setVisible(true);
+            btn_back.setVisible(true);
+        }
+    }
+
+    public void addComment() {
+        if(!txt_newComment.isVisible()) {
+            commentListView.setVisible(false);
+            btn_showComment.setVisible(false);
+            lbl_plName.setText("New Comment");
+            lbl_commentTitle.setVisible(true);
+            txt_newComment.setVisible(true);
+            btn_back.setVisible(true);
         }else{
-            btn_changeName.setVisible(false);
-        }
-    }
-
-    public void goToSong() throws IOException {
-        if(songListView.getSelectionModel().getSelectedItem() != null) {
-            AppData.setCurrentSong(((Song)songListView.getSelectionModel().getSelectedItem()));
-            App.setRoot("play");
-        }
-    }
-
-    public void goToComments() {
-        try {
-            App.setRoot("comments");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void removeSong() {
-        if(songListView.getSelectionModel().getSelectedItem() != null) {
-            try(SongDAO sdao = new SongDAO(((Song)songListView.getSelectionModel().getSelectedItem()))) {
-                plSongs.remove(sdao);
-                try(PlaylistDAO pldao = new PlaylistDAO(AppData.getCurrentPL())) {
-                    pldao.removeSong(sdao);
-                }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    public void changePlaylistName() {
-        if(!txtfld_changeName.isVisible()) {
-            txtfld_changeName.setVisible(true);
-            txtfld_changeName.setText(AppData.getCurrentPL().getName());
-        }else{
-            if(!txtfld_changeName.getText().equals("")) {
-                try (PlaylistDAO pdao = new PlaylistDAO(AppData.getCurrentPL())) {
-                    AppData.getCurrentUser().getPlaylists().remove(pdao);
-                    pdao.setName(txtfld_changeName.getText());
-                    pdao.save();
-                    txtfld_changeName.setText("");
-                    txtfld_changeName.setVisible(false);
-                    AppData.setCurrentPL(pdao);
-                    AppData.getCurrentUser().getPlaylists().add(pdao);
-                    initialize();
+            if(txt_newComment.getText() != null && !txt_newComment.getText().equals("")) {
+                try(CommentDAO cdao = new CommentDAO(new Comment(-1, txt_newComment.getText(), LocalDateTime.now() , AppData.getCurrentUser(), AppData.getCurrentPL()))) {
+                    cdao.save();
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
+                initialize();
             }
+        }
+    }
+
+    public void back() throws IOException {
+        AppData.setCurrentComment(null);
+        if(btn_addComment.isVisible() && btn_showComment.isVisible()) {
+            App.setRoot("playlist");
+        }else{
+            initialize();
         }
     }
 
