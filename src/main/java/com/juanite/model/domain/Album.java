8882dc0c7b860f2c1a09384092d6d6a4fbd30084
Jose Espejo <jespagu2610@g.educaand.es>
2023-11-28 12:33:56 +0100

    First Commit JLEA

--- src/main/java/com/juanite/model/domain/Album.java
+++ src/main/java/com/juanite/model/domain/Album.java
@@ -1,5 +1,6 @@
 package com.juanite.model.domain;
 
+import javax.persistence.*;
 import java.sql.Date;
 import java.time.Instant;
 import java.time.LocalDate;
@@ -7,13 +8,140 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+@Entity
+@Table(name = "Album")
 public class Album {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private int id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "publication")
     private Date publication;
+
+    @Column(name = "photo")
     private String photo;
+
+    @ManyToMany
+    @JoinTable(
+            name = "artist_album",
+            joinColumns = @JoinColumn(name = "id_album"),
+            inverseJoinColumns = @JoinColumn(name = "id_artist")
+    )
     private List<Artist> artists;
+
+    @OneToMany(mappedBy = "album")
     private List<Song> songs;
+
+    public void save(EntityManagerFactory entityManagerFactory) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.persist(this); // Guardar el álbum actual en la base de datos
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public void update(EntityManagerFactory entityManagerFactory) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.merge(this); // Actualizar el álbum actual en la base de datos
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public void remove(EntityManagerFactory entityManagerFactory) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            entityManager.remove(entityManager.contains(this) ? this : entityManager.merge(this)); // Borrar el álbum actual de la base de datos
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    public static Album getById(EntityManagerFactory entityManagerFactory, int id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Album album = null;
+        try {
+            album = entityManager.find(Album.class, id); // Obtener el álbum por su ID
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return album;
+    }
+
+    public static List<Album> getAll(EntityManagerFactory entityManagerFactory) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        List<Album> albums = null;
+        try {
+            TypedQuery<Album> query = entityManager.createQuery("SELECT a FROM Album a", Album.class);
+            albums = query.getResultList(); // Obtener todos los álbumes de la base de datos
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return albums;
+    }
+
+    public static List<Album> getBySong(EntityManagerFactory entityManagerFactory, Song song) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        List<Album> albums = null;
+        try {
+            TypedQuery<Album> query = entityManager.createQuery("SELECT a FROM Album a JOIN a.songs s WHERE s = :song", Album.class);
+            query.setParameter("song", song);
+            albums = query.getResultList(); // Obtener álbumes por canción
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return albums;
+    }
+
+    public static List<Album> getByArtist(EntityManagerFactory entityManagerFactory, Artist artist) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        List<Album> albums = null;
+        try {
+            TypedQuery<Album> query = entityManager.createQuery("SELECT a FROM Album a JOIN a.artists ar WHERE ar = :artist", Album.class);
+            query.setParameter("artist", artist);
+            albums = query.getResultList(); // Obtener álbumes por artista
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            entityManager.close();
+        }
+        return albums;
+    }
 
 
     // Constructor
